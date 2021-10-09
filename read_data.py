3e5a180e58451ce68e487455adcc7e29935fa1bb
Basit Ali <basit@dreambigsemi.com>
2021-10-09 11:29:40 +0500

    Update the api to read from multiple devices.

--- read_data.py
+++ read_data.py
@@ -1,14 +1,16 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-UNIT = 41
+units = [41,42,43,44]
 
 
 def run_sync_client():
     client = ModbusClient(method='rtu', port='COM3', timeout=1,
                           baudrate=9600)
     client.connect()
-    rr = client.read_holding_registers(12, 2, unit=UNIT)
-    print(rr.registers)
+    for unit in units:
+        rr = client.read_holding_registers(12, 2, unit=unit)
+        print(unit)
+        print(rr.registers)
     client.close()
 
 
