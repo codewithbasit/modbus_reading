4a48da0b57a58c121b5460d0e5cbf2689e92e4d2
Basit Ali <basit@dreambigsemi.com>
2021-10-09 11:59:25 +0500

    Add functionality to read configurations from file.

--- read_data.py
+++ read_data.py
@@ -1,18 +1,34 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import json
 
-units = [41,42,43,44]
+settings={}
 
+def read_settings():
+    f = open('settings.json',)
+    data = json.load(f)
+
+    if data:
+        settings = data
+    else:
+        print("Couldn't read settings.")
+    f.close()
 
 def run_sync_client():
-    client = ModbusClient(method='rtu', port='COM3', timeout=1,
-                          baudrate=9600)
+    client = ModbusClient(method=settings['mode'], port=settings['port'], timeout=1,
+                          baudrate=settings['baudrate'])
     client.connect()
-    for unit in units:
-        rr = client.read_holding_registers(11, 2, unit=unit)
-        print(unit)
+    for device in settings['devices']:
+        rr = client.read_holding_registers(device['register_offset'], 
+                                            device['number_of_regs'], 
+                                            unit=device['slave_id'])
+        print(unit=device['slave_id'])
         print(rr.registers)
     client.close()
 
 
 if __name__ == "__main__":
-    run_sync_client()
+    read_settings()
+    if settings:
+        run_sync_client()
+    else:
+        print("Configurations error.")
