82d5d40bc2faf12db9242645352d44f76aa9e882
Basit Ali <basit@dreambigsemi.com>
2021-10-09 13:04:37 +0500

    Fix data reading and storing.

--- read_data.py
+++ read_data.py
@@ -1,5 +1,7 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 import json
+import time
+
 
 def read_settings():
     settings={}
@@ -14,21 +16,28 @@ def read_settings():
 
     return settings
 
+def read_registers(client, device_id, register_offset, num_of_regs):
+    rr = client.read_holding_registers(register_offset, num_of_regs, unit=device_id)
+
+    if rr.registers:
+        return rr.registers
+    else:
+        return None
 def run_sync_client(settings):
     data = []
     device_data = {}
-    
     client = ModbusClient(method=settings['mode'], port=settings['port'], timeout=1,
                           baudrate=settings['baudrate'])
     client.connect()
     for device in settings['devices']:
-        rr = client.read_holding_registers(device['register_offset'], 
-                                            device['number_of_regs'], 
-                                            unit=device['slave_id'])
+        values = read_registers(client,device['slave_id'], 
+                                        device['register_offset'], 
+                                        device['number_of_regs'])
         device_data['Id'] = device['slave_id']
-        device_data['Temperature'] = (rr.registers[0]/100)
-        device_data['Humidity'] = (rr.registers[1]/100)
+        device_data['Temperature'] = (values[0]/10.0)
+        device_data['Humidity'] = (values[1]/10.0)
         data.append(device_data)
+        device_data = {}
 
     print(data)
     client.close()
@@ -37,6 +46,9 @@ def run_sync_client(settings):
 if __name__ == "__main__":
     settings = read_settings()
     if settings:
-        run_sync_client(settings)
+    
+        while True:
+            run_sync_client(settings)
+            time.sleep(5)
     else:
         print("Configurations error.")
