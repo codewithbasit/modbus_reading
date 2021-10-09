bb0f5688c4027e57611d6498ca90efcac9727596
Basit Ali <basit@dreambigsemi.com>
2021-10-09 11:16:16 +0500

    Make the changes for data reading

--- read_data.py
+++ read_data.py
@@ -1,13 +1,14 @@
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-UNIT = 0x1
+UNIT = 41
 
 
 def run_sync_client():
-    client = ModbusClient(method='rtu', port='/dev/pts/3', timeout=1,
+    client = ModbusClient(method='rtu', port='COM3', timeout=1,
                           baudrate=9600)
     client.connect()
-    rr = client.read_holding_registers(1, 1, unit=UNIT)
+    rr = client.read_holding_registers(12, 2, unit=UNIT)
+    print(rr.registers)
     client.close()
 
 
